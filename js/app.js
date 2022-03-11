0b3783437b0451a22370c8d836adee31dd96dd26
nmartoia <nmartoia@edenschool.fr>
2022-03-11 14:59:47 +0100

    modification du fonctionnement du chronometre

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 let div = document.querySelectorAll("div");
 for (let i = 0; i < div.length; i++) {
-    div[i].classList.add("tous");
+    div[i].className = ("tous");
 }
-let heure = 24
-let minute = 00
+let heure1 = 2
+let heure2 = 4
+let minute1 = 0
+let minute2 = 0
 setInterval(function(){
-minute--
-if(minute<0){
-    minute = 59
-    heure--
+minute2--
+if(minute2<0){
+    minute1 --
+    minute2 = 9
 }
-
+else if(minute1>0){
+    heure2--
+    minute1 =5
+}
+else if(heure2<0){
+    heure1--
+    heure2=9
+}
+else if(heure1>0){
+    heure1 = 2
+    heure2 = 4
+    minute1 = 0
+    minute2 = 0
+}
+console.log(heure1,heure2,minute1,minute2)
 },60000)
+if(heure1==2){
+    div[0].className = "droithautbas"
+    div[1].className = "gauchhautbas"
+}
